3eaa6b2e4a57ee7a8d2c8307123e0eb2cb1ab099
wenhw <wen122056@gmail.com>
2023-09-11 16:34:53 +0800

    fix(ipc):benchmarks

--- ipc/benchmarks_test.go
+++ ipc/benchmarks_test.go
@@ -8,6 +8,8 @@ func BenchmarkIPC(b *testing.B) {
 	readableStreamIpc := NewReadableStreamIPCWithDefaultInputStream(CLIENT, SupportProtocol{Raw: true})
 
 	req := request()
+	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
 		_ = readableStreamIpc.Enqueue(req)
 	}
